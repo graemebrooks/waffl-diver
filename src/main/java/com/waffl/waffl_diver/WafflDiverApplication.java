6b3a275a8b4085faefcb3f6115c2208f29b060e4
Kelso Brooks <graemebrooks@gmail.com>
2024-08-31 11:22:33 -0500

    add spring skeleton

---
+++ src/main/java/com/waffl/waffl_diver/WafflDiverApplication.java
@@ -0,0 +1,13 @@
+package com.waffl.waffl_diver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafflDiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafflDiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waffl/waffl_diver/WafflDiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waffl.waffl_diver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WafflDiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
